2a8fa29659449a1578cf6c1363fa2b39b122bce8
Shah Muhammad Qureshi <132233907+ShahMuhammadQureshi419@users.noreply.github.com>
2025-05-01 22:18:41 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,35 +3,61 @@ provider "aws" {
 }
 
 # ✅ Secure IAM Policy - follows least privilege principle
-resource "aws_iam_policy" "secure_policy" {
-  name        = "SecurePolicy"
-  description = "Policy with restricted actions and resources"
+#resource "aws_iam_policy" "secure_policy" {
+#  name        = "SecurePolicy"
+#  description = "Policy with restricted actions and resources"
+#
+#  policy = jsonencode({
+#    Version = "2012-10-17",
+#    Statement = [
+#      {
+#        Effect   = "Allow"
+#        Action   = [
+#          "s3:ListBucket",
+#          "s3:GetObject"
+#        ]
+#        Resource = [
+#          "arn:aws:s3:::my-bucket",
+#          "arn:aws:s3:::my-bucket/*"
+#        ]
+#      },
+#      {
+#        Effect   = "Allow"
+#        Action   = [
+#          "ec2:DescribeInstances",
+#          "ec2:StartInstances"
+#        ]
+#        Resource = "*"
+#      }
+#    ]
+#  })
+#}
+
+
+resource "aws_iam_policy" "secure_limited_policy" {
+  name        = "SecureLimitedPolicy"
+  description = "A policy with scoped permissions"
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
-          "s3:ListBucket",
-          "s3:GetObject"
-        ]
-        Resource = [
-          "arn:aws:s3:::my-bucket",
-          "arn:aws:s3:::my-bucket/*"
-        ]
-      },
-      {
-        Effect   = "Allow"
+        Effect   = "Allow",
         Action   = [
           "ec2:DescribeInstances",
-          "ec2:StartInstances"
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          "arn:aws:s3:::my-bucket",
+          "arn:aws:s3:::my-bucket/*",
+          "arn:aws:ec2:us-east-1:123456789012:instance/*"
         ]
-        Resource = "*"
       }
     ]
   })
 }
+
 
 
 
