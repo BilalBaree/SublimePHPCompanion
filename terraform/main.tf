2c166c7cff48559558aab1cbb0239500ee9b73c1
Shah Muhammad Qureshi <132233907+ShahMuhammadQureshi419@users.noreply.github.com>
2025-05-01 22:20:35 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,7 @@ provider "aws" {
 
 resource "aws_iam_policy" "secure_limited_policy" {
   name        = "SecureLimitedPolicy"
-  description = "A policy with scoped permissions"
+  description = "A policy with scoped permissions ."
 
   policy = jsonencode({
     Version = "2012-10-17",
